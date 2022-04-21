aa547f93bd6270b339dfa2150656cbc89fa942ee
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2022-04-21 10:07:17 +0100

    CHORE :: removed default packages

--- templates/package.js
+++ templates/package.js
@@ -8,22 +8,14 @@ const pkg = (fields) => ({
     "description": fields.description,
     "scripts": {
         "start": `node ${fields.entry}`,
-        "dev": `nodemon ${fields.entry}`
+        "dev": `nodemon ${fields.entry}`,
+        "make":"bash install.sh"
     },
     "dependencies": {
-        "cors": "^2.8.5",
-        "dotenv": "^10.0.0",
-        "express": "~4.16.1",
-        "express-useragent": "^1.0.15",
-        "jsonwebtoken": "^8.5.1",
-        "lodash": "^4.17.21",
-        "nodemailer": "^6.7.2",
-        "otp-generator": "^4.0.0",
-        "sequelize": "^6.13.0",
-        "sequelize-bcrypt": "^1.1.0"
+       
     },
     "devDependencies": {
-        "nodemon": "^2.0.15"
+        
     },
     "repository": {
         "type": "git",
